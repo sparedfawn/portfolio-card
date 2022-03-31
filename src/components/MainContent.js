dfe214bc9434c2ef12ca4e375cf1e32a9edc8ff5
Krzysztof Kalinowski <k.kalinowski2000@gmail.com>
2022-03-31 22:03:06 +0200

    formatting changes

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -10,9 +10,7 @@ const MainContent = () => {
             <h2 className={`font-700 font-size-16 main-title-${theme}`}>
                 About
             </h2>
-            <p
-                className={`font-400 font-size-sm text-gray mt-2 main-discription-${theme}`}
-            >
+            <p className={`font-400 font-size-sm text-gray mt-2 main-discription-${theme}`}>
                 I am a frontend developer with a particular interest in making
                 things simple and automating daily tasks. I try to keep up with
                 security and best practices, and am always looking for new
@@ -22,9 +20,7 @@ const MainContent = () => {
             <h2 className={`font-700 font-size-16 main-title-${theme} mt-15`}>
                 Interests
             </h2>
-            <p
-                className={`font-400 font-size-sm text-gray mt-2 main-discription-${theme}`}
-            >
+            <p className={`font-400 font-size-sm text-gray mt-2 main-discription-${theme}`}>
                 Food expert. Music scholar. Reader. Internet fanatic. Bacon
                 buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee
                 fanatic.
